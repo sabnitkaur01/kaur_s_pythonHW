e96a01e7813f2848d176db38cde89284687eeebf
Sabnit Kaur <s_sabnitkaur@ats-hr420-dm22.fc.ca>
2019-10-29 13:34:47 -0400

    done adding python

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 from random import randint
 
 # choices is an array => an array is a container that can hold multiple values
+# array are 0-based -> first entry is 0, 2nd is 1, 3rd 
 choices = ["rock" , "paper" , "scissors"]
 
 # set the computer variable to one of these choice (0, 1 or 2)
@@ -13,16 +14,41 @@ player = False
 
 while player is False:
     # set player to True
+    print("***************************\n\n")
+    print("choose your weapon!\n\n")
+    print("***************************\n\n")
+
     player = input("choose rock, paper, paper or scissors\n")
 
     print("computer chose", computer, "\n")
     print("player chose", player, "\n")
 
-    if player == "quit":
-     	exit()
+    if player.lower() == "quit":
+        exit()
     elif computer == player:
-	    print("tie! no one wins, play again")
+        print("tie! no one wins, play again")
+    elif player == "rock":
+        if computer == "paper":
+            print("You lose!", computer, "covers", player, "\n")
+        else:
+            print("You win!", player, "smashes through", computer, "\n")
+
+    elif player.lower() == "paper":
+        if computer == "scissors":
+            print("You lose!", computer, "cuts", player, "\n")
+        else:
+            print("You win!", player, "covers", computer, "\n")
+        
+    elif player.lower() == "scissors":
+        if computer == "rock":
+            print("You lose!", computer, "smashes", player, "\n")
+        else:
+            print("You win!", player, "cuts", computer, "\n")
+
+    else:
+        print("That's not a valid choice, try again")        
+     
 
     #need to check all of our conditions after checking for a tie
     player = False
-    computer = choice[randint(0, 2)]
+    computer = choices[randint(0, 2)]
